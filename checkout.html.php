1eefd1b6227ee5f94148b573d3d42c060bac046e
pjotrasss <webserver@apache.xd>
2024-12-25 15:07:04 +0000

    started work on checkout functionality, to be continued, NOT A PRODUCTION VERSION YET

--- checkout.html.php
+++ checkout.html.php
@@ -1,9 +1,16 @@
+<?php
+    require ("scripts/php/checkout.php");
+    require_once ("scripts/php/user_sessions.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="styles/header.css">
         <link rel="stylesheet" href="styles/checkout.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="shortcut icon" href="https://store.steampowered.com/favicon.ico" />
         <title>Checkout | steam</title>
     </head>
     <body>
@@ -17,7 +24,7 @@
         <main>
             <h1>CHEKOUT</h1>
             <form name="checkout_details" action="" method="post">
-                
+                <?php show_checkout_form();?>
             </form>
         </main>
     </body>
--- scripts/php/checkout.php
+++ scripts/php/checkout.php
@@ -1,1 +1,20 @@
 <?php
+require ("checkout_subscripts.php");
+require ("user_sessions.php");
+
+function show_checkout_form() {
+    if (hard_session_validation()) {
+        choose_payment_methods();
+        choose_or_add_billing_address();
+        is_this_a_gift();
+    } else {
+        header('Location: ../../login.html.php');
+    }
+};
+
+
+
+if ($_SERVER['REQUEST_METHOD']==='POST' && isset($_POST['checkout'])) {
+    upload_order_to_db();
+    redirect_to_payment();
+};
